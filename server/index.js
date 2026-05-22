1baebbdd7ceba99cf1a5f5f7fc17dfba957524e3
dante374 <danteruto2008@gmail.com>
2026-05-22 14:14:43 -0300

    cambio en index para tomar vercel

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-    origin: 'https://musicstore-1wrj.vercel.app'
+    origin: '/\.vercel\.app$/'
 }));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
